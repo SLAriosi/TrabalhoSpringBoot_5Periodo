4300cff4f82a863adc516a400c11c119c4d98302
AriosiLucas <ariosilucas@gmail.com>
2025-05-31 14:31:09 -0300

    Projeto Estrutura Inicial Completa

--- medicProgram/src/main/java/equipe/garotosdeprograma/repository/AgendaRepository.java
+++
@@ -1,9 +0,0 @@
-package equipe.garotosdeprograma.repository;
-
-import equipe.garotosdeprograma.model.Agenda;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AgendaRepository extends JpaRepository<Agenda, Long> {
-    List<Agenda> findAllByOrderByDataHoraAsc();
-}
--- medicProgram/src/main/java/equipe/garotosdeprograma/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package equipe.garotosdeprograma.repository;
-
-import equipe.garotosdeprograma.model.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-}
